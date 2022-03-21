3137670c5c00201eccb75eccca6abb3362b4796c
huangyongj2e <867541656@qq.com>
2022-03-21 16:57:34 +0800

    gateway

---
+++ spring-gateway/spring-gateway-labs1/src/main/java/com/cn/llhy/spring/labs/gateway/labs/GatewayApplication.java
@@ -0,0 +1,23 @@
+package com.cn.llhy.spring.labs.gateway.labs;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * @author Huangyong
+ * @version 1.0.0
+ * @Description
+ * @createTime 2022-3-21 16:00
+ */
+@SpringBootApplication
+@EnableAsync
+@ServletComponentScan
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
